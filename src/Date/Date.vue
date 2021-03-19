a6e648ee28a24b30bf57ea279084762b0244e9e4
Matthew Anderson <matthewanderson@Matthews-MacBook-Pro.local>
2021-03-19 16:58:10 +0000

    Adding date component

--- src/Date/Date.vue
+++ src/Date/Date.vue
@@ -1,21 +1,26 @@
 <template>
-    <div v-if="items.weekdayFormat || items.dayFormat || items.monthFormat || items.yearFormat" :class="classes">
-        <p v-if="items.weekdayFormat" :class="weekdayClasses">
-            {{ date[items.weekdayFormat] }}
-        </p>
-        <p v-if="items.dayFormat" :class="dayClasses">
-            {{ date[items.dayFormat] }}
-        </p>
-        <p v-if="items.monthFormat" :class="monthClasses">
-            {{ date[items.monthFormat] }}
-        </p>
-        <p v-if="items.yearFormat" :class="yearClasses">
-            {{ date[items.yearFormat] }}
-        </p>
-    </div>
-    <div v-else :class="classes">
+    <component
+        :style="[styles]"
+        :is="computedTag"
+        :controls="!items.disableControls"
+        :playsinline="items.playsinline" 
+        :autoplay="items.autoplay" 
+        :preload="items.preload" 
+        :muted="items.muted" 
+        :loop="items.loop"
+        :class="classes">
         {{ formatted }}
-    </div>
+        <component
+            v-for="(item, index) in items.data" 
+            :key="index"
+            :content="content"
+            :record="record"
+            :indicator="indicator"
+            :settings="settings"
+            :is="content[item].type"
+            :opts="content[item]">
+        </component>
+    </component>
 </template>
 
 <script>
@@ -31,62 +36,29 @@
         },
         computed: {
             formatted() {
-                if (this.items.format) {
-                    return moment(this.now).format(this.items.format);
-                }
-                return moment().format('dddd Do MMMM YYYY');
+                return moment(this.items.datetime ? this.items.datetime : this.now).format(this.items.format ? this.items.format : 'dddd Do MMMM YYYY');
+            },
+            computedTag() {
+                return this.items.tag ? this.items.tag : 'div'
             },
             classes() {
-                if (typeof this.settings[this.items.type] != 'undefined' && this.settings[this.items.type].length > 0) {
+                if (this.settings[this.items.type] && this.settings[this.items.type].length > 0) {
                     return this.settings[this.items.type];
                 }
                 return this.items.classes;
             },
-            dayClasses() {
-                if (typeof this.settings[this.items.type] != 'undefined' && this.settings[this.items.type].length > 0) {
-                    return this.settings[this.items.type];
-                }
-                return this.items.dayClasses;
-            },
-            weekdayClasses() {
-                if (typeof this.settings[this.items.type] != 'undefined' && this.settings[this.items.type].length > 0) {
-                    return this.settings[this.items.type];
-                }
-                return this.items.weekdayClasses;
-            },
-            monthClasses() {
-                if (typeof this.settings[this.items.type] != 'undefined' && this.settings[this.items.type].length > 0) {
-                    return this.settings[this.items.type];
-                }
-                return this.items.classes;
-            },
-            yearClasses() {
-                if (typeof this.settings[this.items.type] != 'undefined' && this.settings[this.items.type].length > 0) {
-                    return this.settings[this.items.type];
-                }
-                return this.items.yearClasses;
+            styles() {
+                return {
+                    fontFamily: this.items.fontFamily,
+                    minHeight: this.items.minHeight,
+                    backgroundImage: this.items.backgroundImage ? 'url(' + this.items.backgroundImage + ')' : null
+                };
             }
         },
         beforeMount: function () {
             if (this.opts != null) {
                 this.items = this.opts;
             }
-            this.date.d = moment(this.now).format('d');
-            this.date.dd = moment(this.now).format('dd');
-            this.date.ddd = moment(this.now).format('ddd');
-            this.date.dddd = moment(this.now).format('dddd');
-
-            this.date.m = moment(this.now).format('M');
-            this.date.mm = moment(this.now).format('MM');
-            this.date.mmm = moment(this.now).format('MMM');
-            this.date.mmmm = moment(this.now).format('MMMM');
-
-            this.date.D = moment(this.now).format('D');
-            this.date.DD = moment(this.now).format('DD');
-            this.date.DDD = moment(this.now).format('DDD');
-
-            this.date.yy = moment(this.now).format('YY');
-            this.date.yyyy = moment(this.now).format('YYYY');
         }
     }
 </script>
